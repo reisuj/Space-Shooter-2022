b6c8d0c2402e79509427d9d19c52552af9fdca96
REISU-LEOPARD\reisu <reisuj@gmail.com>
2022-08-02 09:21:47 -0400

    Player movement and boundaries complete

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 3.5f;
+
+    [SerializeField]
+    private float yMaxPosition, yMinPosition, xMaxPosition, xMinPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        yMaxPosition = 0.0f;
+        yMinPosition = -5.0f;
+        xMaxPosition = 11.3f;
+        xMinPosition = -11.3f;
+
+        transform.position = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMovement();
+    }
+
+    void PlayerMovement()
+    {
+        float verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.up * Time.deltaTime * _speed * verticalInput);
+
+        float yPosition = Mathf.Clamp(transform.position.y, yMinPosition, yMaxPosition);
+        transform.position = new Vector3(transform.position.x, yPosition, 0);
+
+        float horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * Time.deltaTime * _speed * horizontalInput);
+        if (transform.position.x < xMinPosition)
+        {
+            transform.position = new Vector3(xMaxPosition, transform.position.y, 0);
+        }
+        else if (transform.position.x > xMaxPosition)
+        {
+            transform.position = new Vector3(xMinPosition, transform.position.y, 0);
+        }
+    }
+}
