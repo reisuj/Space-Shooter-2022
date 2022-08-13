dc677d81789d64ad3dd7712e1c84b8c9c516eaa0
REISU-LEOPARD\reisu <reisuj@gmail.com>
2022-08-12 21:36:11 -0400

    Added Speed Powerup and initial modular powerup functionality

--- Assets/Scripts/Powerup.cs
+++ Assets/Scripts/Powerup.cs
@@ -6,6 +6,13 @@ public class Powerup : MonoBehaviour
 {
     [SerializeField]
     private float _speed = 3.0f;
+
+    //ID for Powerups
+    //0 = Triple Shot
+    //1 = Speed
+    //2 = Shields
+    [SerializeField]
+    private int _powerupID;
     // Update is called once per frame
     void Update()
     {
