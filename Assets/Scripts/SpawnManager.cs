75380e2d969b0f11c063f892aa80ebc5b26e0f39
REISU-LEOPARD\reisu <reisuj@gmail.com>
2023-05-31 11:26:47 -0400

    WIP: Adding Wave control for enemy spawning

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -18,6 +18,27 @@ public class SpawnManager : MonoBehaviour
 
     private bool _playerIsAlive = true;
 
+    [SerializeField]
+    private int _waveCurrent = 1;
+    [SerializeField]
+    private int _waveMax = 3;
+    [SerializeField]
+    private int _enemiesInWave = 0;
+    [SerializeField]
+    private int _enemiesSpawned = 0;
+    [SerializeField]
+    private int _enemiesRemaining = 0;
+    private UIManager _uiManager = null;
+
+    private void Start()
+    {
+        _uiManager = GameObject.Find("Canvas").GetComponent<UIManager>();
+        if (_uiManager == null)
+        {
+            Debug.LogError("The UIManager is NULL!");
+        }
+    }
+
     IEnumerator SpawnEnemyRoutine()
     {
         yield return new WaitForSeconds(3.0f);
@@ -50,8 +71,6 @@ public class SpawnManager : MonoBehaviour
     private void PowerUpSelector()
     {
         int weight = Random.Range(1, 111);
-
-
     }
 
     void SpawnPowerup()
@@ -60,6 +79,39 @@ public class SpawnManager : MonoBehaviour
         int randomPowerUp = Random.Range(0, _powerups.Length);
         GameObject newPowerup = Instantiate(_powerups[randomPowerUp], powerupPosition, Quaternion.identity);
         newPowerup.transform.parent = _powerupContainer.transform;
+    }
+
+    private void WaveControl()
+    {
+        if (_waveCurrent <= _waveMax)
+        {
+            if (_enemiesSpawned == _enemiesInWave && _enemiesRemaining == 0)
+            {
+                Debug.Log("Pre Dtsrt Next Wave");
+                _enemiesSpawned = 0;
+                _enemiesInWave += 2;
+                _enemiesRemaining = 0;
+                _waveCurrent++;
+                StartCoroutine(NextWave());
+            }
+        }
+        else
+        {
+            Debug.Log("Waves Ended");
+            StopAllCoroutines();
+        }
+    }
+
+    public void EnemyKilled()
+    {
+        _enemiesRemaining--;
+    }
+
+    IEnumerator NextWave()
+    {
+        yield return new WaitForSeconds(5.0f);
+        StartCoroutine(SpawnEnemyRoutine());
+        Debug.Log("Next Wave Started");
     }
 
     public void StopSpawning()
